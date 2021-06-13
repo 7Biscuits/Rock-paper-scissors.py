c7094642f401b5bcb8a51da11d945c466918676f
BiscuitButB <RudranshSrivastava2525@gmail.com>
2021-06-13 16:34:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,98 +1,96 @@
 import random
 
-random_result = ["Rock", "Paper", "Scissor"]
-user_end = ["end", "End", "END", "finish", "Finish", "FINISH"]
-user_yes = ["Yes", "yes", "YES", "Yeah", "Ofcource", "Yep", "yep", "YEP"]
-user_no = ["No", "no", "NO", "Nope", "NOPE", "nope", "never", "Never", "NEVER"]
+game_objects = ['rock', 'paper', 'scissor']
+player_name = input('Enter your name: ')
+rounds = int(input('Enter the number of rounds: '))
+reply_yes = ['y', 'yes', 'yeah', 'yup', 'yep']
 
-def main():
-    user_points = 0
+def roundsAreZero():
+    warning = print('You cannot enter 0 or negetive integers. The minimum number of rounds is 1')
+    return warning
+
+if rounds <= 0:
+    roundsAreZero()
+
+elif rounds >= 1:
+    pass
+
+def gameLogic():
+    player_points = 0
     computer_points = 0
+    for i in range(rounds):
+        player_move = input('rock, paper or scissor? ').lower()
+        computer_move = random.choice(game_objects)
+        if player_move == computer_move:
+            print(computer_move)
+            print('Its a tie!')
+            print(f"computer's points: {computer_points}")
+            print(f"{player_name}'s points: {player_points}")
 
-    while True:
-        user_ask = input("Rock, Paper or Scissor? ")
-        result = random.choice(random_result)
-        
-        if user_ask == result:
-            print("Tie!!")
-            print("Your move: " + user_ask)
-            print("Computer's move: " + result)
-            print("Your points: ", user_points)
-            print("Computer's points: ", computer_points)
-
-        elif user_ask == "Paper" and result == "Rock":
-            print("You won!")
-            print("Your move: " + user_ask)
-            print("Computer's move: " + result)
-            user_points += 1
-            print("Your points: ", user_points)
-            print("Computer's points: ", computer_points)
-
-        elif user_ask == "Paper" and result == "Scissor":
-            print("You lost")
-            print("Your move: " + user_ask)
-            print("Computer's move: " + result)
+        elif player_move == 'rock' and computer_move == 'paper':
+            print(computer_move)
+            print('computer won!')
             computer_points += 1
-            print("Your points: ", user_points)
-            print("Computer's points: ", computer_points)
+            print(f"{player_name}'s points: {player_points}")
+            print(f"computer's points: {computer_points}")
 
-        elif user_ask == "Rock" and result == "Paper":
-            print("You lost")
-            print("Your move: " + user_ask)
-            print("Computer's move: " + result)
+        elif player_move == 'rock' and computer_move == 'scissor':
+            print(computer_move)
+            print(f"{player_name} won!!")
+            player_points += 1
+            print(f"user's points: {player_points}")
+
+        elif player_move == 'paper' and computer_move == 'rock':
+            print(computer_move)
+            print(f"{player_name} won!!")
+            player_points += 1
+            print(f"user's points: {player_points}")
+
+        elif player_move == 'paper' and computer_move == 'scissor':
+            print(computer_move)
+            print('computer won!')
             computer_points += 1
-            print("Your points: ", user_points)
-            print("Computer's points: ", computer_points)
+            print(f"{player_name}'s points: {player_points}")
+            print(f"computer's points: {computer_points}")
 
-        elif user_ask == "Rock" and result == "Scissor":
-            print("You won!!")
-            print("Your move: " + user_ask)
-            print("Computer's move: " + result)
-            user_points += 1
-            print("Your points: ", user_points)
-            print("Computer's points: ", computer_points)
+        elif player_move == 'scissor' and computer_move == 'paper':
+            print(computer_move)
+            print(f"{player_name} won!!")
+            player_points += 1
+            print(f"user's points: {player_points}")
 
-        elif user_ask == "Scissor" and result == "Rock":
-            print("You lost")
-            print("Your move: " + user_ask)
-            print("Computer's move: " + result)
+        elif player_move == 'scissor' and computer_move == 'rock':
+            print(computer_move)
+            print('computer won!')
             computer_points += 1
-            print("Your points: ", user_points)
-            print("Computer's points: ", computer_points)
+            print(f"{player_name}'s points: {player_points}")
+            print(f"computer's points: {computer_points}")
 
-        elif user_ask == "Scissor" and result == "Paper":
-            print("You won!")
-            print("Your move: " + user_ask)
-            print("Computer's move: " + result)
-            user_points += 1
-            print("Your points: ", user_points)
-            print("Computer's points: ", computer_points)
+        elif player_move == 'end':
+            if player_points > computer_points:
+                print(f"{player_name} is the winner")
+                print(f"{player_name}'s points: {player_points}")
+                print(f"computer's points: {computer_points}")
 
-        elif user_ask in user_end:
-            if user_points > computer_points:
-                print("You have won against computer")
-                print("Your points: ", user_points)
-                print("Computer's points: " + computer_points)
-
-            elif user_points < computer_points:
-                print("You have lost against computer")
-                print("Your points: ", user_points)
-                print("Computer's points: " + computer_points)
+            elif player_points < computer_points:
+                print('computer won!')
+                print(f"{player_name}'s points: {player_points}")
+                print(f"computer's points: {computer_points}")
 
             else:
-                print("Its a tie between computer and You")
-                print("Your points: ", user_points)
-                print("Computer's points: " + computer_points)
+                print('Match Tied!!')
+                print(f"{player_name}'s points: {player_points}")
+                print(f"computer's points: {computer_points}")
             break
 
         else:
-            print("Invalid Input")
-            user_error_ask = input("Will you like to try again? ")
+            print('Invalid Input')
+            user_playAgain = input('Wanna play again? ').lower()
+            if user_playAgain in reply_yes:
+                gameLogic()
 
-            if user_error_ask in user_yes:
-                return main()
-            
-            elif user_error_ask in user_no:
+            else:
                 break
-            
-main()
+
+if rounds >= 1:
+    gameLogic()
